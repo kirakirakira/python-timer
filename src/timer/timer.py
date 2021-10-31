3988946c36e6da3d44df652f360f0bf99fe51355
Kira Hartlage <kira.hartlage@gmail.com>
2021-10-31 16:09:43 -0400

    need to get time to pass

--- src/timer/timer.py
+++ src/timer/timer.py
@@ -8,6 +8,8 @@ class Timer(object):
     def __init__(self, interval):
         self._interval = interval
         self._status = Timer._STOPPED
+        # self._callback = callback
+        self.finished = False
 
     @property
     def status(self):
@@ -16,7 +18,18 @@ class Timer(object):
     def start(self):
         self._start_time = time.monotonic()
     
-    @property
+    @property # not use a property?
     def remaining_ticks(self):
+        # need to make the mock update time.monotonic in here
         elapsed_time = time.monotonic() - self._start_time
+        print("inside remaining ticks", elapsed_time)
+        if(self._interval - elapsed_time == 0):
+            self._finished = True
         return self._interval - elapsed_time
+
+    def run(self):
+        print("inside run", self.remaining_ticks)
+        while self.remaining_ticks > 0:
+            if self.remaining_ticks == 0:
+                break
+        self._finished = True
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -6,24 +6,32 @@ from tests.time_mock import Time_Mock
 timey = Time_Mock()
 
 class TestTimer(unittest.TestCase):
-    def test_init(self):
-        new_timer = Timer(5)
-        self.assertEqual(new_timer.status, Timer._STOPPED)
+    # def test_init(self):
+    #     new_timer = Timer(5)
+    #     self.assertEqual(new_timer.status, Timer._STOPPED)
 
-    @mock.patch('time.monotonic', timey.current_time)
-    def test_start(self):
+    # @mock.patch('time.monotonic', timey.current_time)
+    # def test_start(self):
+    #     new_timer = Timer(5)
+    #     new_timer.start()
+    #     self.assertEqual(new_timer.remaining_ticks, 5)
+
+    # @mock.patch('time.monotonic', timey.current_time)
+    # def test_elapse_time(self):
+    #     new_timer = Timer(5)
+    #     new_timer.start()
+    #     timey.elapse(1)
+    #     self.assertEqual(new_timer.remaining_ticks, 4)
+    #     timey.elapse(2)
+    #     self.assertEqual(new_timer.remaining_ticks, 2)
+
+    @mock.patch('time.monotonic', timey.current_time)  # time needs to go by...
+    def test_finish_timing(self):
         new_timer = Timer(5)
         new_timer.start()
-        self.assertEqual(new_timer.remaining_ticks, 5)
-
-    @mock.patch('time.monotonic', timey.current_time)
-    def test_elapse_time(self):
-        new_timer = Timer(5)
-        new_timer.start()
-        timey.elapse(1)
-        self.assertEqual(new_timer.remaining_ticks, 4)
-        timey.elapse(2)
-        self.assertEqual(new_timer.remaining_ticks, 2)
+        timey.elapse(5)
+        new_timer.run()
+        self.assertEqual(new_timer.finished, True)
 
 if __name__ == '__main__':
     unittest.main()
