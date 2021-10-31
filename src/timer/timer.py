f9c737f85750e5a200440a367d4c53b4cf001992
Kira Hartlage <kira.hartlage@gmail.com>
2021-10-31 15:19:28 -0400

    remaining ticks

--- src/timer/timer.py
+++ src/timer/timer.py
@@ -5,8 +5,14 @@ class Timer(object):
     def __init__(self, interval):
         self._interval = interval
         self._status = Timer._STOPPED
-        self._start_time = 0
 
     @property
     def status(self):
         return self._status
+
+    def start(self):
+        self._start_time = 0
+    
+    @property
+    def remaining_ticks(self):
+        return self._interval - self._start_time
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -7,5 +7,10 @@ class TestTimer(unittest.TestCase):
         new_timer = Timer(5)
         self.assertEqual(new_timer.status, Timer._STOPPED)
 
+    def test_start(self):
+        new_timer = Timer(5)
+        new_timer.start()
+        self.assertEqual(new_timer.remaining_ticks, 5)
+
 if __name__ == '__main__':
     unittest.main()
