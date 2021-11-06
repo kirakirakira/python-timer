65630457e4b31a5bee513f243e523cf8db1f4351
Kira Hartlage <kira.hartlage@gmail.com>
2021-11-06 16:50:26 -0400

    remove from timer list

--- src/timer/timer_collection.py
+++ src/timer/timer_collection.py
@@ -11,22 +11,32 @@ class TimerCollection(object):
 
         @property
         def remaining_ticks(self):
+            # need to make sure not less than 0
             elapsed_time = time.monotonic() - self._start_time
-            return self._interval - elapsed_time
+            remaining_ticks = self._interval - elapsed_time
 
-    def __init__(self) -> None:
-        super().__init__()
+            if remaining_ticks < 0:
+                return 0
+            else:
+                return remaining_ticks
+
+    def __init__(self):
         self.timers = []
 
-    def add_timer(self, interval, fn):
+    def _add_timer(self, interval, fn):
         timer = self.Timer(interval, fn)
         self.timers.append(timer)
         return timer
 
     def start_timer(self, interval, fn):
-        return self.add_timer(interval, fn)
+        return self._add_timer(interval, fn)
 
     def run(self):
+        timers_to_remove = []
         for timer in self.timers:
             if timer.remaining_ticks == 0:
                 timer._fn()
+                timers_to_remove.append(timer)
+        
+        for timer in timers_to_remove:
+            self.timers.remove(timer)
--- tests/test_timer_collection.py
+++ tests/test_timer_collection.py
@@ -67,6 +67,77 @@ class TestTimerCollection(unittest.TestCase):
         self.assertEqual(called_back_a[0], True)
         self.assertEqual(called_back_b[0], True)
 
+    @mock.patch('time.monotonic', timey.current_time)
+    def test_remaining_ticks_is_only_positive(self):
+        called_back = [False]
+
+        def callback():
+            called_back[0] = True
+
+        new_timer_collection = TimerCollection()
+        timer = new_timer_collection.start_timer(5, callback)
+        self.assertEqual(timer.remaining_ticks, 5)
+
+        timey.elapse(5)
+        new_timer_collection.run()
+        self.assertEqual(called_back[0], True)
+        self.assertEqual(timer.remaining_ticks, 0)
+
+        timey.elapse(1)
+        self.assertEqual(timer.remaining_ticks, 0)
+
+    @mock.patch('time.monotonic', timey.current_time)
+    def test_remove_finished_timer_from_list(self):
+        called_back = [0]
+
+        def callback():
+            called_back[0] += 1
+
+        new_timer_collection = TimerCollection()
+        timer = new_timer_collection.start_timer(5, callback)
+        self.assertEqual(timer.remaining_ticks, 5)
+
+        timey.elapse(5)
+        new_timer_collection.run()
+        self.assertEqual(called_back[0], 1)
+
+        timey.elapse(5)
+        new_timer_collection.run()
+        self.assertEqual(called_back[0], 1)
+
+    @mock.patch('time.monotonic', timey.current_time)
+    def test_multiple_timers_that_finish_at_different_times(self):
+        called_back_a = [0]
+
+        def callback_a():
+            called_back_a[0] += 1
+
+        called_back_b = [0]
+
+        def callback_b():
+            called_back_b[0] += 1
+
+        new_timer_collection = TimerCollection()
+        timer_a = new_timer_collection.start_timer(6, callback_a)
+        self.assertEqual(timer_a.remaining_ticks, 6)
+        timey.elapse(2)
+
+        timer_b = new_timer_collection.start_timer(3, callback_b)
+        self.assertEqual(timer_a.remaining_ticks, 4)
+        self.assertEqual(timer_b.remaining_ticks, 3)
+
+        timey.elapse(3)
+        new_timer_collection.run()
+        self.assertEqual(called_back_b[0], 1)
+
+        timey.elapse(1)
+        new_timer_collection.run()
+        self.assertEqual(called_back_a[0], 1)
+
+        timey.elapse(6)
+        self.assertEqual(called_back_a[0], 1)
+        self.assertEqual(called_back_b[0], 1)
+
 
 if __name__ == '__main__':
     unittest.main()
