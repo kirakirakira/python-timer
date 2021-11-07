eba60cb4ff5b9d36e2135987b118fadb210d0e64
Kira Hartlage <kira.hartlage@gmail.com>
2021-11-07 16:30:33 -0500

    blink those leds, holiday style :tree:

--- blinker.py
+++ blinker.py
@@ -4,21 +4,25 @@ from timer_collection import TimerCollection
 
 cp.pixels.brightness = 0.01
 
+clear = (0, 0, 0)
 pink = (250, 50, 174)
 blue = (0, 0, 255)
 green = (0, 255, 0)
-clear = (0, 0, 0)
+red = (255, 0, 0)
+teal = (0, 255, 255)
+yellow = (255, 255, 0)
+orange = (255, 30, 30)
+
 
 timer_collection = TimerCollection()
 
 cp.red_led = False
-cp.pixels[0] = clear
-cp.pixels[1] = clear
-cp.pixels[2] = clear
+
+for pixel in range(10):
+    cp.pixels[pixel] = clear
 
 def red_led_callback():
     cp.red_led = not cp.red_led
-    print(cp.red_led)
 
 def pixel_callback(*args):
     index = args[0][0]
@@ -29,10 +33,40 @@ def pixel_callback(*args):
     else:
         cp.pixels[index] = clear
 
-timer_red_led = timer_collection.start_periodic_timer(1, red_led_callback)
-timer_pixel_0 = timer_collection.start_periodic_timer(2, pixel_callback, (0, pink))
-timer_pixel_1 = timer_collection.start_periodic_timer(3, pixel_callback, (1, blue))
-timer_pixel_2 = timer_collection.start_periodic_timer(4, pixel_callback, (2, green))
+timer_collection.start_periodic_timer(1, red_led_callback)
+
+timer_configs = [
+    {"interval": 0.2, "led": (0, pink)},
+    {"interval": 0.3, "led": (1, blue)},
+    {"interval": 0.4, "led": (2, yellow)},
+    {"interval": 0.5, "led": (3, red)},
+    {"interval": 0.6, "led": (4, teal)},
+    {"interval": 0.5, "led": (5, orange)},
+    {"interval": 0.4, "led": (6, blue)},
+    {"interval": 0.3, "led": (7, pink)},
+    {"interval": 0.2, "led": (8, green)},
+    {"interval": 0.6, "led": (9, red)}
+]
+
+christmas_configs = [
+    {"interval": 0.2, "led": (0, red)},
+    {"interval": 0.3, "led": (1, green)},
+    {"interval": 0.4, "led": (2, red)},
+    {"interval": 0.5, "led": (3, green)},
+    {"interval": 0.6, "led": (4, red)},
+    {"interval": 0.5, "led": (5, green)},
+    {"interval": 0.4, "led": (6, red)},
+    {"interval": 0.3, "led": (7, green)},
+    {"interval": 0.2, "led": (8, red)},
+    {"interval": 0.6, "led": (9, green)}
+]
+
+for timer_config in christmas_configs:
+    timer_collection.start_periodic_timer(
+        timer_config["interval"],
+        pixel_callback,
+        timer_config["led"]
+    )
 
 while True:
     time_until_next = timer_collection.run()
