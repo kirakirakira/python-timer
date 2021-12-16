d0ff2d82d6191652f434d8ade109278f51cd3ae5
Kira Hartlage <kira.hartlage@gmail.com>
2021-12-16 15:11:05 -0500

    add debouncing

--- accelerometer_alarm.py
+++ accelerometer_alarm.py
@@ -11,16 +11,21 @@ difference = {"delta x": 0, "delta y": 0, "delta z": 0}
 timer_collection = TimerCollection()
 
 acceleration_monitor_timer = [None]
+acceleration_monitor_limit = [0]
+debounce = [0]
 
 def blink_led():
     cp.red_led = not cp.red_led
 
 def youre_done():
     cp.play_file("burp_x.wav")
-    button_pressed = 0
-    print("YOU ARE DONE 1347183O4134")
+    acceleration_monitor_limit[0] = 0
+    print("YOU ARE DONE")
 
 def print_acceleration():
+    print(debounce[0])
+    print(acceleration_monitor_limit[0])
+
     acceleration = {"x": cp.acceleration.x, "y": cp.acceleration.y, "z": cp.acceleration.z}
     difference["delta x"] = abs(acceleration["x"] - previous_acceleration["x"])
     difference["delta y"] = abs(acceleration["y"] - previous_acceleration["y"])
@@ -31,10 +36,15 @@ def print_acceleration():
     print("difference", difference)
 
     if difference["delta x"] < 0.5 and difference["delta y"] < 0.5 and difference["delta z"] < 0.5:
-        print("YOU ARE THE BESTEST **********")
-        print(acceleration_monitor_timer[0])
+        print("DIFFERENCE IS LOW ENOUGH")
+        debounce[0] += 1
+    else:
+        debounce[0] = 0
+
+    if debounce[0] >= 5:
         timer_collection.stop(acceleration_monitor_timer[0])
         timer_collection.start_timer(5, youre_done)
+
     previous_acceleration["x"] = acceleration["x"]
     previous_acceleration["y"] = acceleration["y"]
     previous_acceleration["z"] = acceleration["z"]
@@ -42,18 +52,15 @@ def print_acceleration():
 blink_led_timer = timer_collection.start_periodic_timer(0.2, blink_led)
 acceleration_monitor_timer[0] = timer_collection.start_periodic_timer(2, print_acceleration)
 
-button_pressed = 0
 
 while True:
     time_until_next = timer_collection.run()
 
     if cp.button_a:
-        print("BUTTON A IS PRESSED =======")
-        button_pressed += 1
-        print(button_pressed)
+        acceleration_monitor_limit[0] += 1
 
-        if button_pressed == 1:
-            print("A BUTTON TIMER STARTED")
+        if acceleration_monitor_limit[0] == 1:
+            debounce[0] = 0
             acceleration_monitor_timer[0] = timer_collection.start_periodic_timer(2, print_acceleration)
 
     if time_until_next != None:
