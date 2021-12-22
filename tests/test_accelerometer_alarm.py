aaf97a02aee0286473da44b0f65b55454247792a
Kira Hartlage <kira.hartlage@gmail.com>
2021-12-22 17:44:50 -0500

    repo cleanup

--- tests/test_accelerometer_alarm.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.timer.timer_collection import TimerCollection
-from unittest import mock
-from tests.time_mock import Time_Mock
-
-timey = Time_Mock()
-
-# class TestAccelerometerAlarm(unittest.TestCase):
-#     def test_init(self):
